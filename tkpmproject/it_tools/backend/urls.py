f8f953cc81b62f77fe4150d26c9008df7f0ecc66
codefulTM <nguyentuanminh435@gmail.com>
2025-04-17 19:05:46 +0700

    Added the logout route that implements the logout function

--- tkpmproject/it_tools/backend/urls.py
+++ tkpmproject/it_tools/backend/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name="login"),
     path('signup/', views.signup, name="signup"),
+    path('logout/', views.logout, name='logout'),
     path('', views.it_tools, name="it_tools")
 ]
