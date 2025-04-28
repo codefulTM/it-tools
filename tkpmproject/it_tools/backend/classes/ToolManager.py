c1c50da6363580e18eeb77ed30bebc0387beb225
codefulTM <nguyentuanminh435@gmail.com>
2025-04-28 09:43:34 +0700

    Implement dynamic tool loading in ToolManager and enhance tool selection in the frontend

--- tkpmproject/it_tools/backend/classes/ToolManager.py
+++ tkpmproject/it_tools/backend/classes/ToolManager.py
@@ -1,10 +1,29 @@
+import os
+import importlib
+from classes.Tool import Tool
+
 class ToolManager:
     def __init__(self):
-        pass
+        self.tools = None
+
+        # From the files in the "tools" folder, get all the classes that extend the Tool 
+        # class and add objects of those classes to the tools list
+        folder_name = "../tools"
+        for filename in os.listdir(folder_name):
+            if filename.endswith(".py"):
+                module_name = filename[:-3]
+                module_path = f"{folder_name}.{module_name}".replace("/", "")  
+                module = importlib.import_module(module_path)
+
+                for attr_name in dir(module):
+                    attr = getattr(module, attr_name)
+                    if isinstance(attr, type) and issubclass(attr, Tool) and attr is not Tool:
+                        self.tools.append(attr())
+        
 
     def add_tool(self, tool_config, html_file, js_file):                
-        # Get the content of the html_file
-
+        # Get the content of the html_file 
+        
         # Get the content of the js_file
 
         # Create the content of the new file that includes the class that extends the Tool class
