5f5b68a78f1eb51264a71adc351e26f986b7695a
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-05-01 18:19:09 +0700

    Create tool class for the Port Generator

---
+++ tkpmproject/it_tools/backend/tools/Tool30.py
@@ -0,0 +1,61 @@
+from backend.classes.ToolComponent import ToolComponent
+from data_service.services.tool_services import get_tool_by_id
+
+class Tool30(ToolComponent):
+    def get_info(self):
+        info = get_tool_by_id(30)
+
+        if (info != None):
+            self.name = info.name
+            self.description = info.description
+            self.category = info.category.name
+            self.is_premium = info.is_premium
+            self.is_enabled = info.is_enabled
+            
+            return {
+                'id': 30,
+                'name': self.name,
+                'category': self.category,
+                'is_premium': self.is_premium,
+                'is_enabled': self.is_enabled
+            }
+        
+        else:
+            print("Error: Cannot retrieve the info for tool id 30")
+            return None
+        
+    def get_html(self):
+        return f"""
+            <h2 class="tool-name">{self.name}</h2>
+            <p class="tool-des">{self.description}</p>
+
+            <div class="tool-body">
+                <p class="random-port" id="port" name="port"></p>
+            </div>
+            <input type="button" value="Generate port" class="normal-btn" id="generate-port">
+            <input type="button" value="Refresh port" class="normal-btn" id="refresh-port">
+        """
+    
+    def get_js(self):
+        return """
+            const randomPort = document.getElementById("port");
+            const generatePort = document.getElementById("generate-port");
+            const refreshPort = document.getElementById("refresh-port");
+            
+            const defaultPort = 10000;
+            const fromPort = 1024;
+            const toPort = 65535;
+
+            randomPort.textContent = defaultPort;
+
+            generatePort.addEventListener ("click", () => {
+                randomPort.textContent = Math.floor((Math.random() * (toPort - fromPort))) + fromPort // From 1024 to 99999
+            });
+
+            refreshPort.addEventListener ("click", () => {
+                randomPort.textContent = defaultPort;
+            })
+        """
+    
+    def get_package(self):
+        return ""
