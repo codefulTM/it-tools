4c08840445bcc0df8a3121db105868e75f33921c
codefulTM <nguyentuanminh435@gmail.com>
2025-05-03 12:44:08 +0700

    - In the get_premium api, get user id from a parameter instead of using session information

--- tkpmproject/it_tools/backend/views/api_users.py
+++ tkpmproject/it_tools/backend/views/api_users.py
@@ -3,10 +3,11 @@ from data_service.services.user_role_services import *
 from data_service.services.user_services import * 
 from ..utils import *
 
-def get_premium(request):
-    user_id, username, user_role = get_user_from_session(request)
-    
+def get_premium(request, user_id):
     user = get_user_by_id(user_id)
+
+    if user == None:
+        return JsonResponse({'success': False, 'message': 'User does not exist.'})
 
     if user.is_premium == True:
         return JsonResponse({'success': False, 'message': 'You are already a premium user.'})
