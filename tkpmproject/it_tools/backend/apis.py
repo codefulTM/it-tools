4481b4e223fc383b9bf982e4ebbff517ca4d88f0
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-05-03 14:04:22 +0700

    Add toggle favorite tool for account/premium/admin users
    Create function to toggle favorite tools and get all favorite tools in backend
    Add icon for user to mark or unmark the favorite tools

--- tkpmproject/it_tools/backend/apis.py
+++ tkpmproject/it_tools/backend/apis.py
@@ -4,6 +4,8 @@ from .views import api_tools
 urlpatterns = [
     path('tools/', api_tools.get_tools, name="api_tools"),
     path('categories/<int:category_id>/', api_tools.get_tools_by_category, name="api_get_tools_by_category"),
+    path('favorites/toggle/<int:user_id>/<int:tool_id>/', api_tools.toggle_favorite_tool, name="api_toggle_favorite_tool"),
+    path('favorites/<int:user_id>/', api_tools.get_all_favorites, name="api_get_all_favorites"),
     path('tools/<int:tool_id>/toggleenable/', api_tools.toggle_tool_status, name="api_toggle_tool_status"),
     path('tools/<int:tool_id>/togglepremium/', api_tools.toggle_tool_premium, name="api_toggle_tool_premium"),
     path('tools/<int:tool_id>/deletetool/', api_tools.delete_tool, name="api_delete_tool"),
--- tkpmproject/it_tools/backend/views/api_tools.py
+++ tkpmproject/it_tools/backend/views/api_tools.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from data_service.services.tool_services import *  # assuming this is the model for tools
 from backend.classes.ToolManager import ToolManager
+from backend.classes.User import AccountUser
 
 def get_tools(request):
     """
@@ -65,3 +66,24 @@ def delete_tool(request, tool_id):
         return JsonResponse({"success": delete_result, "message": delete_message})
     except Exception as e:
         return JsonResponse({"success": False, "message": {str(e)}})
+         
+def toggle_favorite_tool(request, user_id, tool_id):
+    try:
+        add_result = AccountUser.toggle_favorite(user_id, tool_id)
+        if (add_result):
+            return JsonResponse({"success": True, "message": "Add favorite successfully"})
+        else:
+            return JsonResponse({"success": False, "message": "Failed to add favorite tool"})
+    except Exception as e:
+        return JsonResponse({"success": False, "message": str(e)})
+
+def get_all_favorites(request, user_id):
+    try:
+        favorites = AccountUser.get_favorite(user_id)
+        if (favorites != None):
+            favorite_tool_ids = [favorites.tool_id for favorite in favorites]
+            return JsonResponse({"success": True, "favorites": favorite_tool_ids})
+        else:
+            return JsonResponse({"success": False, "message": "Failed to get all favorite tools"})
+    except Exception as e:
+        return JsonResponse({"success": False, "message": str(e)})
